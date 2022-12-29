906f0b3b735f96ac7ab2e91d8c6d0fc99baeae2f
andrermilof <andrermilof@gmail.com>
2022-12-29 08:50:56 +0300

    Добавьте файлы проекта.

---
+++ exer3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+
+namespace exer3
+{
+
+    public class Currency
+    {
+        public static double USDcoef;
+        public static double EURcoef;
+        public virtual double Value { get; set; }
+    }
+
+    public class CurrencyUSD : Currency
+    {
+        public override double Value { get; set; }
+        public CurrencyUSD() { Value = 0; }
+        public CurrencyUSD(CurrencyUSD ur)
+        {
+            Value = ur.Value;
+        }
+
+        public static implicit operator CurrencyUSD(CurrencyRUB val)
+        {
+            return new CurrencyUSD { Value = val.Value / USDcoef };
+        }
+
+        public static implicit operator CurrencyUSD(CurrencyEUR val)
+        {
+            return new CurrencyUSD { Value = val.Value * EURcoef / USDcoef };
+        }
+    }
+
+    public class CurrencyEUR : Currency
+    {
+        public override double Value { get; set; }
+        public CurrencyEUR() { Value = 0; }
+        public CurrencyEUR(CurrencyEUR er)
+        {
+            Value = er.Value;
+        }
+
+        public static implicit operator CurrencyEUR(CurrencyUSD val)
+        {
+            return new CurrencyEUR { Value = val.Value * USDcoef / EURcoef };
+        }
+
+        public static implicit operator CurrencyEUR(CurrencyRUB val)
+        {
+            return new CurrencyEUR { Value = val.Value / EURcoef };
+        }
+    }
+
+    public class CurrencyRUB : Currency
+    {
+        public override double Value { get; set; }
+        public CurrencyRUB() { Value = 0; }
+        public CurrencyRUB(CurrencyRUB rb)
+        {
+            Value = rb.Value;
+        }
+
+        public static implicit operator CurrencyRUB(CurrencyEUR val)
+        {
+            return new CurrencyRUB { Value = EURcoef * val.Value };
+        }
+
+        public static implicit operator CurrencyRUB(CurrencyUSD val)
+        {
+            return new CurrencyRUB { Value = USDcoef * val.Value };
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter USD to RUB:");
+            Currency.USDcoef = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter EUR to RUB:");
+            Currency.EURcoef = Convert.ToDouble(Console.ReadLine());
+
+            CurrencyUSD us;
+            CurrencyEUR er;
+            CurrencyRUB rb = new CurrencyRUB();
+
+            rb.Value = 100;
+            us = rb;
+            er = rb;
+            
+
+            Console.WriteLine($"RUB:{rb.Value}\tEUR:{er.Value}\tUSD:{us.Value}");
+
+            us.Value = 100;
+            rb = us;
+            er = rb;
+
+            Console.WriteLine($"RUB:{rb.Value}\tEUR:{er.Value}\tUSD:{us.Value}");
+
+
+            us.Value = 1000;
+            rb = us;
+            er = us;
+
+            Console.WriteLine($"RUB:{rb.Value}\tEUR:{er.Value}\tUSD:{us.Value}");
+        }
+    }
+}
---
+++ lab03/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exer1
+{
+    public class Vector
+    {
+        private double x, y, z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double Length 
+        { 
+            get { return Math.Sqrt(x * x + y * y + z * z); } 
+        }
+
+        public static Vector operator + (Vector v1, Vector v2)
+        {
+            return new Vector(v2.x + v1.x, v2.y + v1.y, v2.z + v1.z);
+        }
+
+        public static Vector operator * (Vector v, double n)
+        {
+            return new Vector(v.x * n, v.y * n, v.z * n);
+        }
+
+        public static Vector operator * (double n, Vector v)
+        {
+            return v * n;
+        }
+
+        public static double operator * (Vector v1, Vector v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+
+        public static bool operator < (Vector v1, Vector v2)
+        {
+            return v1.Length < v2.Length;
+        }
+
+        public static bool operator > (Vector v1, Vector v2)
+        {
+            return v1.Length > v2.Length;
+        }
+
+        public static bool operator == (Vector v1, Vector v2)
+        {
+            return v1.Length == v2.Length;
+        }
+
+        public static bool operator != (Vector v1, Vector v2)
+        {
+            return v1.Length != v2.Length;
+        }
+
+        public static bool operator <= (Vector v1, Vector v2)
+        {
+            return v1.Length <= v2.Length;
+        }
+
+        public static bool operator >= (Vector v1, Vector v2)
+        {
+            return v1.Length >= v2.Length;
+        }
+    }
+
+    public class Car : IEquatable<Car>
+    {
+        public string Name { get; set; }
+        public string Engine { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object c)
+        {
+            return this == c as Car;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ MaxSpeed.GetHashCode() ^ Engine.GetHashCode();
+        }
+
+        public bool Equals(Car c)
+        {
+            return Name == c.Name && Engine == c.Engine && MaxSpeed == c.MaxSpeed;
+        }
+    }
+
+    public class CarsCatalog
+    {
+        private List<Car> cars;
+
+        public CarsCatalog(List<Car> args)
+        {
+            cars = args;
+        }
+
+        public string this[int index]
+        {
+            get
+            {   
+                return $"{cars[index].Name} {cars[index].Engine}";
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1, 3, 4);
+            Vector v2 = new Vector(1, 3, 4);
+
+            if (v1 == v2)
+                Console.WriteLine("v1 == v2");
+
+            double v = v2 * v1;
+            Console.WriteLine(v);
+        }
+    }
+}
